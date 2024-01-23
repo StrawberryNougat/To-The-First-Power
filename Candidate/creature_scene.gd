cae5303eca461b9289f45df29cfa91ad1a38606a
Tvandersall1244 <157185358+Tvandersall1244@users.noreply.github.com>
2024-01-23 15:34:16 -0500

    Created invincibility frames

--- Candidate/creature_scene.gd
+++ Candidate/creature_scene.gd
@@ -3,11 +3,21 @@ extends CharacterBody2D
 var current_form
 var speed = 300
 var health = 5
+var invincible = false
+var timer 
 @onready var image = get_node("creature_current_design")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	image.speed_up.connect(speed_up)
+	
+	#create timer node on entering scene tree and set timer conditions
+	var newtimer = Timer.new()
+	add_child(newtimer)
+	newtimer.wait_time = .5
+	newtimer.one_shot = true
+	newtimer.autostart = false
+	newtimer.timeout.connect(_on_timer_timeout)
 
 func _process(_delta):
 	var direction = Input.get_vector("left","right","up","down")
@@ -18,5 +28,13 @@ func speed_up():
 	speed = 500
 
 func hit():
-	health = health - 1
-	print("Hit!")
+	if !invincible:
+		health = health - 1
+		print("Hit!")
+		invincible = true
+		timer.start(.5)
+	else:
+		health = health
+	
+func _on_timer_timeout():
+	invincible = false
