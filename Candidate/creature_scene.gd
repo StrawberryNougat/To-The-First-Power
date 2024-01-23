64a4fa3cb9eda514f0943b7ecd18dbb6ca56b43a
Reedric <86981900+Reedric@users.noreply.github.com>
2024-01-23 00:05:01 -0500

    Candidate Bullets

--- Candidate/creature_scene.gd
+++ Candidate/creature_scene.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+const bullet_scene = preload("res://fight_scenes/bullets/basic_bullet.tscn")
 var current_form
 var speed = 300
 var health = 5
@@ -20,3 +21,12 @@ func speed_up():
 func hit():
 	health = health - 1
 	print("Hit!")
+
+func _unhandled_input(_event):
+	if Input.is_action_just_pressed("interact") and get_tree().current_scene.name == "first_fight":
+		var bullet = bullet_scene.instantiate()
+		get_tree().root.add_child(bullet)
+		bullet.position = position + Vector2(40.0, 0.0) # Sets position 
+		bullet.rotation = rotation
+		bullet.apply_scale(Vector2(2, 2)) # Sets scale of bullet, <1,1> is original size
+		return
