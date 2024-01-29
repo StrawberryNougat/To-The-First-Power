b0f73252c57065e21f0d081124aa02e6dc4a36df
StrawberryNougat <eleanormather02@gmail.com>
2024-01-28 23:01:26 -0500

    added hitbox vendor, fixed strength_up signal bug

--- dialogue/dialogue_conditions.gd
+++ dialogue/dialogue_conditions.gd
@@ -3,9 +3,12 @@ extends Node
 var opening_choice = ""
 signal chose_strength
 signal chose_agility
+signal chose_visible_hitbox
 
 func _process(_delta):
 	if (opening_choice == "strength"):
 		emit_signal("chose_strength")
 	elif (opening_choice == "speed"):
 		emit_signal("chose_agility")
+	elif (opening_choice == "vis_hitbox"):
+		emit_signal("chose_visible_hitbox")
