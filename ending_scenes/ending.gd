ec99b9a4356536e852d7ffe50bbf7b651fcf7860
StrawberryNougat <eleanormather02@gmail.com>
2024-02-24 16:39:28 -0500

    creature resets after beating the game

--- ending_scenes/ending.gd
+++ ending_scenes/ending.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-
+var form
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	form = get_node("/root/dialogue_conditions")
 	pass # Replace with function body.
 
 
@@ -12,4 +13,7 @@ func _process(delta):
 
 func _on_exit_body_entered(body):
 	print("going to credits...")#pass # Replace with function body.
+	form.opening_choice = ""
+	form.second_choice = ""
+	#reset creature powers before restarting game
 	get_tree().change_scene_to_file("res://UI/credits.tscn")
